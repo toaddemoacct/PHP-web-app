9c971ff83e684efe9fca3adb5865131d3ad27fb8
toaddemoacct <toaddemoacct@gmail.com>
2015-05-13 15:32:40 -0700

    added ECHO to bat file

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
+echo on
 copy /Y "C:\Program Files (x86)\Jenkins\jobs\PHP Demo\workspace\http_root\*.*" z:\Apache24\htdocs\
-"C:\Program Files\Dell\Toad for Oracle 12.7\toad.exe" -a "Code Analysis"
+toad -a "Code Analysis"
+exit
