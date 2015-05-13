012bff799aabc7387106b25129b98581ec3a3571
toaddemoacct <toaddemoacct@gmail.com>
2015-05-13 15:44:26 -0700

    changing MyFunction

--- http_root/func.php
+++ http_root/func.php
@@ -1,6 +1,6 @@
 <?php
 
-$c = oci_connect('phphol', 'welcome', '//localhost/orcl');
+$c = oci_connect('phphol', 'welcome', '//localhost/pdb_demo');
 $s = oci_parse($c, "begin :bv := myfunc('mydata', 123); end;");
 oci_bind_by_name($s, ":bv", $v, 10);
 oci_execute($s);
